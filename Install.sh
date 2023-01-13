c95038e11990f77745c7209a3c3852eed3770209
Eto_Yoshimura <122558267+3TO3TO@users.noreply.github.com>
2023-01-14 00:16:07 +0630

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,8 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install git -y
+sudo apt install python3-pip -y
+sudo pip3 install requests pysocks
+sudo apt install neofetch -y
+chmod +x DDoS_Attack.py
+clear
+neofetch
